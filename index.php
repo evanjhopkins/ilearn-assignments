618ff20c258c7d842a446c0ea5b2de5611730b77
EvanJHopkins <moviemaker7@gmail.com>
2015-02-21 20:17:21 +0000

    made menu marist red

--- index.php
+++ index.php
@@ -4,14 +4,15 @@ session_start();
 <html>
 <head>
 </head>
+<link rel="stylesheet" href="css.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css">
-<link rel="stylesheet" href="css.css">
 <script src="https://code.jquery.com/jquery-2.1.3.min.js"</script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 <body>
-<nav class="navbar navbar-default">
+
+<nav class="navbar navbar-custom">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">Pretty iLearn</a>
@@ -29,6 +30,8 @@ session_start();
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+
+
 <?php
 
 if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password']) ){
